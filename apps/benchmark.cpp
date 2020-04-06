f7f6b01648a9b2e77b4a8b53014e5f6acd9dbcab
Stefan Hillmich <stefan.hillmich@jku.at>
2020-04-06 13:36:21 +0200

    Fixed label of benchmark

--- apps/benchmark.cpp
+++ apps/benchmark.cpp
@@ -58,7 +58,7 @@ static void BM_sim_TOFFOLI(benchmark::State& state) {
     for(auto _ : state) {
         sim.Simulate();
     }
-    state.SetLabel("TOFFOLI");
+    state.SetLabel("Toffoli");
 }
 BENCHMARK(BM_sim_TOFFOLI)->DenseRange(4, 25);
 
@@ -112,6 +112,8 @@ static void BM_sim_QCBM(benchmark::State& state) {
     state.SetLabel("QCBM");
 }
 
+BENCHMARK(BM_sim_QCBM)->DenseRange(4, 25);
+
 static void BM_extra_QCBM_optimized(benchmark::State& state) {
     const unsigned int n_qubits = state.range(0);
     const unsigned int depth = 9;
@@ -124,54 +126,54 @@ static void BM_extra_QCBM_optimized(benchmark::State& state) {
         qc_pre->emplace_back<qc::StandardOperation>(n_qubits, i, qc::RX, 1.0);
         qc_pre->emplace_back<qc::StandardOperation>(n_qubits, i, qc::RZ, 1.0);
     }
-
-
-    // entangling
-    for (int i=0; i < n_qubits; i++) {
-        qc_pre->emplace_back<qc::StandardOperation>(n_qubits, qc::Control(i), (i+1)%n_qubits, qc::X);
-    }
     dd::Edge pre{qc_pre->buildFunctionality(dd)};
     dd->incRef(pre);
+    std::cout << "pre=" << dd->size(pre) << "  ";
 
-    // middle part: rotations and entanglement
+    // entangling
+    std::unique_ptr<qc::QuantumComputation> qc_entangle = std::make_unique<qc::QuantumComputation>(n_qubits);
+    for (int i=0; i < n_qubits; i++) {
+        qc_entangle->emplace_back<qc::StandardOperation>(n_qubits, qc::Control(i), (i+1)%n_qubits, qc::X);
+    }
+    dd::Edge entangle{qc_entangle->buildFunctionality(dd)};
+    dd->incRef(entangle);
+    std::cout << "entangle=" << dd->size(entangle) << "  ";
 
     // mid rotation
     std::unique_ptr<qc::QuantumComputation> qc_middle = std::make_unique<qc::QuantumComputation>(n_qubits);
-
     for (int i=0; i < n_qubits; i++) {
         qc_middle->emplace_back<qc::StandardOperation>(n_qubits, i, qc::RZ, 1.0);
         qc_middle->emplace_back<qc::StandardOperation>(n_qubits, i, qc::RX, 1.0);
         qc_middle->emplace_back<qc::StandardOperation>(n_qubits, i, qc::RZ, 1.0);
     }
-    // entangling
-    for (int i=0; i < n_qubits; i++) {
-        qc_middle->emplace_back<qc::StandardOperation>(n_qubits, qc::Control(i), (i+1)%n_qubits, qc::X);
-    }
     dd::Edge middle{qc_middle->buildFunctionality(dd)};
     dd->incRef(middle);
+    std::cout << "middle=" << dd->size(middle) << "  ";
 
     // last rotation
     std::unique_ptr<qc::QuantumComputation> qc_post = std::make_unique<qc::QuantumComputation>(n_qubits);
-
     for (int i=0; i < n_qubits; i++) {
         qc_post->emplace_back<qc::StandardOperation>(n_qubits, i, qc::RZ, 1.0);
         qc_post->emplace_back<qc::StandardOperation>(n_qubits, i, qc::RX, 1.0);
     }
     dd::Edge post{qc_post->buildFunctionality(dd)};
     dd->incRef(post);
+    std::cout << "post=" << dd->size(post) << "\n";
 
     for(auto _ : state) {
         dd::Edge qstate = dd->makeZeroState(n_qubits);
         qstate = dd->multiply(pre, qstate);
+        qstate = dd->multiply(entangle, qstate);
 
         for (int d=0; d < depth-1; d++) {
             qstate = dd->multiply(middle, qstate);
+            qstate = dd->multiply(entangle, qstate);
             dd->garbageCollect();
         }
         qstate = dd->multiply(post, qstate);
-        benchmark::DoNotOptimize(qstate);
         dd->garbageCollect();
     }
+    state.SetLabel("QCBM preprocessed");
 }
 
 BENCHMARK(BM_extra_QCBM_optimized)->DenseRange(4, 25);
