62a19482c3a3c7383d276963077125866d5ab3c7
Stefan Hillmich <hillmich@users.noreply.github.com>
2021-06-04 15:00:16 +0200

    Extend the Qiskit Provider/Backend (#24)
    * Import assembled QasmQobjExperiment instead of QuantumCircuit (via QFR, thanks @burgholzer )
    * Add unit tests to test python code
    * Add jkq/* folder to clang-format checks
    * Updated README.md
    * Unified workflow for bindings (one workflow with conditional job to publish to PyPI)
    * StatevectorSimulator now inherits from QasmSimulator and only sets some class variables (inpired by Qiskit's BasicAer)
    * Properly handle barriers in CircuitSimulator (i.e., ignore them)

--- jkq/ddsim/qasmsimulator.py
+++ jkq/ddsim/qasmsimulator.py
@@ -3,11 +3,15 @@
 import logging
 import time
 import uuid
+import warnings
+from typing import Union, List
 
 from qiskit.providers import BackendV1, Options
-from qiskit import qobj, QiskitError
+from qiskit import QiskitError, QuantumCircuit
 from qiskit.providers.models import BackendConfiguration, BackendStatus
+from qiskit.qobj import QasmQobjExperiment, Qobj, QasmQobj, PulseQobj
 from qiskit.result import Result
+from qiskit.compiler import assemble
 
 from .jkqjob import JKQJob
 from jkq import ddsim
@@ -18,9 +22,15 @@ logger = logging.getLogger(__name__)
 class QasmSimulator(BackendV1):
     """Python interface to JKQ DDSIM"""
 
+    SHOW_STATE_VECTOR = False
+
     @classmethod
     def _default_options(cls) -> Options:
-        return Options(shots=None)
+        return Options(
+            shots=None,
+            parameter_binds=None,
+            simulator_seed=None,
+        )
 
     def __init__(self, configuration=None, provider=None):
         conf = {
@@ -51,53 +61,66 @@ class QasmSimulator(BackendV1):
             'open_pulse': False,
             'gates': []
         }
-        if configuration:
-            conf.update(configuration)
-        super().__init__(configuration=BackendConfiguration.from_dict(conf), provider=provider)
+        super().__init__(configuration=configuration or BackendConfiguration.from_dict(conf), provider=provider)
 
-    def run(self, quantum_circuit, **options):
-        if isinstance(quantum_circuit, qobj.QasmQobj) or isinstance(quantum_circuit, qobj.PulseQobj):
+    def run(self, quantum_circuits: Union[QuantumCircuit, List[QuantumCircuit]], **options):
+        if isinstance(quantum_circuits, QasmQobj) or isinstance(quantum_circuits, PulseQobj):
             raise QiskitError('QasmQobj and PulseQobj are not supported.')
+
+        if not isinstance(quantum_circuits, list):
+            quantum_circuits = [quantum_circuits]
+
+        out_options = {}
+        for key in options:
+            if not hasattr(self.options, key):
+                warnings.warn("Option %s is not used by this backend" % key, UserWarning, stacklevel=2)
+            else:
+                out_options[key] = options[key]
+        circuit_qobj = assemble(quantum_circuits, self, **out_options)
+
         job_id = str(uuid.uuid4())
-        local_job = JKQJob(self, job_id, self._run_job, quantum_circuit, **options)
+        local_job = JKQJob(self, job_id, self._run_job, circuit_qobj, **options)
         local_job.submit()
         return local_job
 
-    def _run_job(self, job_id, experiments, **options):
-        self._validate(experiments)
-
-        if not isinstance(experiments, list):
-            experiments = [experiments]
+    def _run_job(self, job_id, qobj_instance: Qobj, **options):
+        self._validate(qobj_instance)
 
         start = time.time()
-        result_list = [self.run_experiment(quantum_circuit, **options) for quantum_circuit in experiments]
+        result_list = [self.run_experiment(qobj_exp, **options) for qobj_exp in qobj_instance.experiments]
         end = time.time()
+
         result = {'backend_name': self.configuration().backend_name,
                   'backend_version': self.configuration().backend_version,
-                  'qobj_id': '0',
+                  'qobj_id': qobj_instance.qobj_id,
                   'job_id': job_id,
                   'results': result_list,
                   'status': 'COMPLETED',
                   'success': True,
-                  'time_taken': (end - start)}
+                  'time_taken': (end - start),
+                  'header': qobj_instance.header.to_dict()
+                  }
         return Result.from_dict(result)
 
-    def run_experiment(self, quantum_circuit, **options):
+    def run_experiment(self, qobj_experiment: QasmQobjExperiment, **options):
         start_time = time.time()
-
-        sim = ddsim.CircuitSimulator(quantum_circuit)
+        sim = ddsim.CircuitSimulator(qobj_experiment, options.get('seed', -1))
         counts = sim.simulate(options['shots'])
         end_time = time.time()
+        counts_hex = {hex(int(result, 2)): count for result, count in counts.items()}
 
-        return {'header': {'name': quantum_circuit.name},
-                'name': quantum_circuit.name,
-                'status': 'DONE',
-                'time_taken': end_time - start_time,
-                'seed': options['shots'],
-                'shots': 1,
-                'data': {'counts': counts},
-                'success': True
-                }
+        result = {'header': qobj_experiment.header.to_dict(),
+                  'name': qobj_experiment.header.name,
+                  'status': 'DONE',
+                  'time_taken': end_time - start_time,
+                  'seed': options.get('seed', -1),
+                  'shots': options['shots'],
+                  'data': {'counts': counts_hex},
+                  'success': True,
+                  }
+        if self.SHOW_STATE_VECTOR:
+            result['data']['statevector'] = sim.get_vector()
+        return result
 
     def _validate(self, quantum_circuit):
         return
---
+++ test/python/test_standalone.py
@@ -0,0 +1,31 @@
+import unittest
+
+from qiskit import *
+
+from jkq import ddsim
+
+
+class StandaloneTests(unittest.TestCase):
+    def test_standalone(self):
+        circ = QuantumCircuit(3)
+        circ.h(0)
+        circ.cx(0, 1)
+        circ.cx(0, 2)
+
+        sim = ddsim.CircuitSimulator(circ)
+        result = sim.simulate(1000)
+        self.assertEqual(len(result.keys()), 2)
+        self.assertIn('000', result.keys())
+        self.assertIn('111', result.keys())
+
+    def test_standalone_with_seed(self):
+        circ = QuantumCircuit(3)
+        circ.h(0)
+        circ.cx(0, 1)
+        circ.cx(0, 2)
+
+        sim = ddsim.CircuitSimulator(circ, 1337)
+        result = sim.simulate(1000)
+        self.assertEqual(len(result.keys()), 2)
+        self.assertIn('000', result.keys())
+        self.assertIn('111', result.keys())
