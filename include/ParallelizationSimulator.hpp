6a8abcbbc4ed275ab9a4e343302c6c907488f78b
Lukas Burgholzer <lukas.burgholzer@jku.at>
2021-07-08 12:35:31 +0200

    🐛 fix code errors
    Signed-off-by: Lukas Burgholzer <lukas.burgholzer@jku.at>

--- include/ParallelizationSimulator.hpp
+++ include/ParallelizationSimulator.hpp
@@ -23,10 +23,12 @@ public:
     std::map<std::string, std::size_t> Simulate(unsigned int shots) override;
 
 protected:
+    std::unique_ptr<dd::Package> ddM = std::make_unique<dd::Package>();
+
     // declare any methods that are used internally in the simulator here
-    static __gnu_cxx::__normal_iterator<const std::unique_ptr<qc::Operation>*, std::vector<std::unique_ptr<qc::Operation>>> getWarmUpDepth(const std::unique_ptr<qc::QuantumComputation>& qc, std::unique_ptr<dd::Package>& ddPackage);
-    static qc::VectorDD simulateMatrixVectorBasic(const std::unique_ptr<qc::QuantumComputation>& qc, const qc::VectorDD& initialState, std::unique_ptr<dd::Package>& ddPackage, __gnu_cxx::__normal_iterator<const std::unique_ptr<qc::Operation>*, std::vector<std::unique_ptr<qc::Operation>>> iter);
-    static qc::MatrixDD simulateMatrixMatrixBasic(const std::unique_ptr<qc::QuantumComputation>& qc, const qc::MatrixDD& initialMatrix, std::unique_ptr<dd::Package>& ddPackage, __gnu_cxx::__normal_iterator<const std::unique_ptr<qc::Operation>*, std::vector<std::unique_ptr<qc::Operation>>> iter);
+    static std::vector<std::unique_ptr<qc::Operation>>::iterator getWarmUpDepth(const std::unique_ptr<qc::QuantumComputation>& qc, std::unique_ptr<dd::Package>& ddPackage);
+    static qc::VectorDD                                          simulateMatrixVectorBasic(const std::unique_ptr<qc::QuantumComputation>& qc, const qc::VectorDD& initialState, std::unique_ptr<dd::Package>& ddPackage, std::vector<std::unique_ptr<qc::Operation>>::iterator iter);
+    static qc::MatrixDD                                          simulateMatrixMatrixBasic(const std::unique_ptr<qc::QuantumComputation>& qc, const qc::MatrixDD& initialMatrix, std::unique_ptr<dd::Package>& ddPackage, std::vector<std::unique_ptr<qc::Operation>>::iterator iter);
     // declare any member needed by the simulator here
     // such as the (potentially synchronized) queue for the computation, or the thread pool for the simulation
 };
--- src/ParallelizationSimulator.cpp
+++ src/ParallelizationSimulator.cpp
@@ -10,11 +10,11 @@ std::map<std::string, std::size_t> ParallelizationSimulator::Simulate(unsigned i
     ddM->incRef(initialMatrix);
     auto warmupdepth = ParallelizationSimulator::getWarmUpDepth(qc, dd);
     // asynchronously call the simulation routine with the quantum computation, the initial state, and the already initialized DD package
-    auto vector = std::async(std::launch::async, [this, initialState]() {
+    auto vector = std::async(std::launch::async, [&]() {
         return ParallelizationSimulator::simulateMatrixVectorBasic(qc, initialState, dd, warmupdepth);
     });
-    auto matrix = std::async(std::launch::async, [this, initialMatrix](){
-        return ParallelizationSimulator::simulateMatrixMatrixBasic(qc, initialMatrix,ddM, warmupdepth);
+    auto matrix = std::async(std::launch::async, [&]() {
+        return ParallelizationSimulator::simulateMatrixMatrixBasic(qc, initialMatrix, ddM, warmupdepth);
     });
     std::cout << "simulation has started" << std::endl;
 
@@ -32,42 +32,41 @@ std::map<std::string, std::size_t> ParallelizationSimulator::Simulate(unsigned i
     return {};
 }
 
-qc::VectorDD ParallelizationSimulator::simulateMatrixVectorBasic(const std::unique_ptr<qc::QuantumComputation>& qc, const qc::VectorDD& initialState, std::unique_ptr<dd::Package>& ddPackage,__gnu_cxx::__normal_iterator<const std::unique_ptr<qc::Operation>*, std::vector<std::unique_ptr<qc::Operation>>> iter) {
+qc::VectorDD ParallelizationSimulator::simulateMatrixVectorBasic(const std::unique_ptr<qc::QuantumComputation>& qc, const qc::VectorDD& initialState, std::unique_ptr<dd::Package>& ddPackage, std::vector<std::unique_ptr<qc::Operation>>::iterator iter) {
     qc::VectorDD state = initialState;
-    for (auto ptr = qc->begin(); ptr != iter; ptr++){
-            if (!ptr->get()->isUnitary()) {
-                throw std::runtime_error("Non-unitary operation encountered. This is not supported for now. Aborting simulation");
-            }
-            auto tmp = ddPackage->multiply(ptr->get()->getDD(ddPackage), state);
-            ddPackage->incRef(tmp);
-            ddPackage->decRef(state);
-            state = tmp;
-            ddPackage->garbageCollect();
+    for (auto ptr = qc->begin(); ptr != iter; ptr++) {
+        if (!ptr->get()->isUnitary()) {
+            throw std::runtime_error("Non-unitary operation encountered. This is not supported for now. Aborting simulation");
+        }
+        auto tmp = ddPackage->multiply(ptr->get()->getDD(ddPackage), state);
+        ddPackage->incRef(tmp);
+        ddPackage->decRef(state);
+        state = tmp;
+        ddPackage->garbageCollect();
     }
     return state;
 }
 
-qc::MatrixDD ParallelizationSimulator::simulateMatrixMatrixBasic(const std::unique_ptr<qc::QuantumComputation>& qc, const qc::MatrixDD& initialMatrix, std::unique_ptr<dd::Package>& ddPackage, decltype(qc->begin())& iter) {
+qc::MatrixDD ParallelizationSimulator::simulateMatrixMatrixBasic(const std::unique_ptr<qc::QuantumComputation>& qc, const qc::MatrixDD& initialMatrix, std::unique_ptr<dd::Package>& ddPackage, std::vector<std::unique_ptr<qc::Operation>>::iterator iter) {
     qc::MatrixDD matrix = initialMatrix;
-    for (auto ptr =iter; ptr != qc->end();ptr++){
-            if (!ptr->get()->isUnitary()) {
-                throw std::runtime_error("Non-unitary operation encountered. This is not supported for now. Aborting simulation");
-            }
-            auto tmp = ddPackage->multiply(ptr->get()->getDD(ddPackage), matrix);
-            ddPackage->incRef(tmp);
-            ddPackage->decRef(matrix);
-            matrix = tmp;
-            ddPackage->garbageCollect();
+    for (auto ptr = iter; ptr != qc->end(); ptr++) {
+        if (!ptr->get()->isUnitary()) {
+            throw std::runtime_error("Non-unitary operation encountered. This is not supported for now. Aborting simulation");
         }
+        auto tmp = ddPackage->multiply(ptr->get()->getDD(ddPackage), matrix);
+        ddPackage->incRef(tmp);
+        ddPackage->decRef(matrix);
+        matrix = tmp;
+        ddPackage->garbageCollect();
+    }
     return matrix;
 }
 
-std::vector<std::unique_ptr<qc::Operation>>::iterator getWarmUpDepth(const std::unique_ptr<qc::QuantumComputation>& qc, std::unique_ptr<dd::Package>& ddPackage) {
+std::vector<std::unique_ptr<qc::Operation>>::iterator ParallelizationSimulator::getWarmUpDepth(const std::unique_ptr<qc::QuantumComputation>& qc, std::unique_ptr<dd::Package>& ddPackage) {
     // create the iterator
-    std::size_t n = qc->size();
-    auto ptr = qc->begin();
-    std::advance(ptr, floor(n/2));
+    std::size_t n   = qc->size();
+    auto        ptr = qc->begin();
+    std::advance(ptr, floor(n / 2));
 
     return ptr;
-
 }
