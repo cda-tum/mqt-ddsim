528654571e1aecfe5b31a455d7395838f5e12a45
Stefan Hillmich <stefan.hillmich@jku.at>
2021-08-30 17:58:29 +0200

    Ignore tests for coverage

--- test/test_hybridsim.cpp
+++ test/test_hybridsim.cpp
@@ -56,20 +56,7 @@ TEST(HybridSimTest, GRCSTestDD) {
     auto result = dd->deserialize<dd::Package::vNode>("result_parallel.dd", true);
     auto ref    = dd->deserialize<dd::Package::vNode>("result.dd", true);
 
-    bool equal = (result == ref);
-    if (!equal) {
-        // if edges are not equal -> compare amplitudes
-        auto refAmplitudes    = dd->getVector(ref);
-        auto resultAmplitudes = dd->getVector(result);
-        equal                 = true;
-        for (std::size_t i = 0; i < refAmplitudes.size(); ++i) {
-            if (std::abs(refAmplitudes[i].real() - resultAmplitudes[i].real()) > 1e-6 || std::abs(refAmplitudes[i].imag() - resultAmplitudes[i].imag()) > 1e-6) {
-                equal = false;
-                break;
-            }
-        }
-    }
-    EXPECT_TRUE(equal);
+    EXPECT_EQ(result, ref);
 }
 
 TEST(HybridSimTest, GRCSTestAmplitudes) {
