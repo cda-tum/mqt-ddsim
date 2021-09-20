741022a2cee9efa2cd6aa033c45ad4647462eb0f
AlexPloier <alexploier@gmail.com>
2021-09-20 12:42:13 +0200

    :bug: change in setup.py - kahypar version to 1.1.6

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
         'opt_einsum>=3.3',
         'quimb>=1.3',
         'autoray>=0.2.5',
-        'kahypar @ https://github.com/kahypar/kahypar/archive/refs/tags/1.1.0.zip',
+        'kahypar >= 1.1.6',
         'sparse>=0.13',
         'python-igraph>=0.9.6',
         'optuna>=2.9.1',
