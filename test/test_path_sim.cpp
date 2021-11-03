d0251dc55423ca05087a38857e4d5f3f652499a6
AlexPloier <alexploier@gmail.com>
2021-11-03 12:33:51 +0100

    :zap: added test case to cover more lines of code

--- test/test_path_sim.cpp
+++ test/test_path_sim.cpp
@@ -106,6 +106,27 @@ TEST(TaskBasedSimTest, GroverCircuitBracket) {
     }
 }
 
+TEST(TaskBasedSimTest, SimpleCircuitBracket) {
+    auto qc = std::make_unique<qc::QuantumComputation>(2);
+    qc->h(1U);
+    qc->x(0U, 1_pc);
+    qc->x(0U, 1_pc);
+    qc->x(0U, 1_pc);
+
+    // construct simulator and generate sequential contraction plan
+    PathSimulator tbs(std::move(qc), PathSimulator::Mode::BracketGrouping3, 1);
+
+    // simulate circuit
+    auto counts = tbs.Simulate(1024);
+
+    EXPECT_TRUE(tbs.dd->getValueByPath(tbs.root_edge, 0).approximatelyEquals({dd::SQRT2_2, 0}));
+    EXPECT_TRUE(tbs.dd->getValueByPath(tbs.root_edge, 3).approximatelyEquals({dd::SQRT2_2, 0}));
+
+    for (const auto& [state, count]: counts) {
+        std::cout << state << ": " << count << std::endl;
+    }
+}
+
 TEST(TaskBasedSimTest, GroverCircuitBracketApproxInfo) {
     std::unique_ptr<qc::QuantumComputation> qc          = std::make_unique<qc::Grover>(4, 12345);
     auto                                    grover      = dynamic_cast<qc::Grover*>(qc.get());
