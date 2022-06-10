ae1d98847570f14a4edc42ed4872dd3b1dde3360
Stefan Hillmich <stefan.hillmich@jku.at>
2022-06-10 11:43:25 +0200

    Fixed Lukas' comments

--- mqt/ddsim/bindings.cpp
+++ mqt/ddsim/bindings.cpp
@@ -11,8 +11,8 @@
 #include "qiskit/QuantumCircuit.hpp"
 
 #include <pybind11/numpy.h>
-#include "pybind11/pybind11.h"
-#include "pybind11/stl.h"
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include <memory>
 // clang-format on
