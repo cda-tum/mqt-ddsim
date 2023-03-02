c109b84baf6a2db5bff9c3812728352adc1fb748
Stefan Hillmich <stefan.hillmich@jku.at>
2023-03-02 12:30:45 +0100

    Clang-Tidy grover header

--- src/GroverSimulator.cpp
+++ src/GroverSimulator.cpp
@@ -8,48 +8,48 @@
 template<class Config>
 std::map<std::string, std::size_t> GroverSimulator<Config>::simulate(std::size_t shots) {
     // Setup X on the last, Hadamard on all qubits
-    qc::QuantumComputation qc_setup(n_qubits + n_anciallae);
-    qc_setup.x(n_qubits);
-    for (qc::Qubit i = 0; i < n_qubits; ++i) {
+    qc::QuantumComputation qc_setup(nQubits + nAnciallae);
+    qc_setup.x(nQubits);
+    for (qc::Qubit i = 0; i < nQubits; ++i) {
         qc_setup.h(i);
     }
 
     dd::Edge setup_op{dd::buildFunctionality(&qc_setup, Simulator<Config>::dd)};
 
     // Build the oracle
-    qc::QuantumComputation qc_oracle(n_qubits + n_anciallae);
+    qc::QuantumComputation qc_oracle(nQubits + nAnciallae);
     qc::Controls           controls{};
-    for (qc::Qubit i = 0; i < n_qubits; i++) {
+    for (qc::Qubit i = 0; i < nQubits; i++) {
         controls.emplace(qc::Control{i, oracle.at(i) == '1' ? qc::Control::Type::Pos : qc::Control::Type::Neg});
     }
-    qc_oracle.z(n_qubits, controls);
+    qc_oracle.z(nQubits, controls);
 
     dd::Edge oracle_op{dd::buildFunctionality(&qc_oracle, Simulator<Config>::dd)};
 
     // Build the diffusion stage.
-    qc::QuantumComputation qc_diffusion(n_qubits + n_anciallae);
+    qc::QuantumComputation qc_diffusion(nQubits + nAnciallae);
 
-    for (qc::Qubit i = 0; i < n_qubits; ++i) {
+    for (qc::Qubit i = 0; i < nQubits; ++i) {
         qc_diffusion.h(i);
     }
-    for (qc::Qubit i = 0; i < n_qubits; ++i) {
+    for (qc::Qubit i = 0; i < nQubits; ++i) {
         qc_diffusion.x(i);
     }
 
-    qc_diffusion.h(n_qubits - 1);
+    qc_diffusion.h(nQubits - 1);
 
     qc::Controls diff_controls{};
-    for (qc::Qubit j = 0; j < n_qubits - 1; ++j) {
+    for (qc::Qubit j = 0; j < nQubits - 1; ++j) {
         diff_controls.emplace(qc::Control{j});
     }
-    qc_diffusion.x(n_qubits - 1, diff_controls);
+    qc_diffusion.x(nQubits - 1, diff_controls);
 
-    qc_diffusion.h(n_qubits - 1);
+    qc_diffusion.h(nQubits - 1);
 
-    for (qc::Qubit i = 0; i < n_qubits; ++i) {
+    for (qc::Qubit i = 0; i < nQubits; ++i) {
         qc_diffusion.x(i);
     }
-    for (qc::Qubit i = 0; i < n_qubits; ++i) {
+    for (qc::Qubit i = 0; i < nQubits; ++i) {
         qc_diffusion.h(i);
     }
 
@@ -58,8 +58,8 @@ std::map<std::string, std::size_t> GroverSimulator<Config>::simulate(std::size_t
     dd::Edge full_iteration{Simulator<Config>::dd->multiply(oracle_op, diffusion_op)};
     Simulator<Config>::dd->incRef(full_iteration);
 
-    assert(n_qubits + n_anciallae <= std::numeric_limits<dd::QubitCount>::max());
-    Simulator<Config>::rootEdge = Simulator<Config>::dd->makeZeroState(static_cast<dd::QubitCount>(n_qubits + n_anciallae));
+    assert(nQubits + nAnciallae <= std::numeric_limits<dd::QubitCount>::max());
+    Simulator<Config>::rootEdge = Simulator<Config>::dd->makeZeroState(static_cast<dd::QubitCount>(nQubits + nAnciallae));
     Simulator<Config>::rootEdge = Simulator<Config>::dd->multiply(setup_op, Simulator<Config>::rootEdge);
     Simulator<Config>::dd->incRef(Simulator<Config>::rootEdge);
 
