fa3c0a6951c3323a13dac4d6cd160a07f72216ca
Stefan Hillmich <stefan.hillmich@jku.at>
2023-03-02 08:43:48 +0100

    Clang-Tidy for Bindings

--- mqt/ddsim/bindings.cpp
+++ mqt/ddsim/bindings.cpp
@@ -44,12 +44,12 @@ static qc::QuantumComputation importCircuit(const py::object& circ) {
 }
 
 template<class Simulator, typename... Args>
-std::unique_ptr<Simulator> create_simulator(const py::object&   circ,
-                                            const double        stepFidelity,
-                                            const unsigned int  stepNumber,
-                                            const std::string&  approximationStrategy,
-                                            const long long int seed,
-                                            Args&&... args) {
+std::unique_ptr<Simulator> createSimulator(const py::object&  circ,
+                                           const double       stepFidelity,
+                                           const unsigned int stepNumber,
+                                           const std::string& approximationStrategy,
+                                           const std::int64_t seed,
+                                           Args&&... args) {
     auto       qc     = std::make_unique<qc::QuantumComputation>(importCircuit(circ));
     const auto approx = ApproximationInfo{stepFidelity, stepNumber, ApproximationInfo::fromString(approximationStrategy)};
     if constexpr (std::is_same_v<Simulator, PathSimulator<>>) {
@@ -60,18 +60,17 @@ std::unique_ptr<Simulator> create_simulator(const py::object&   circ,
             return std::make_unique<Simulator>(std::move(qc),
                                                approx,
                                                std::forward<Args>(args)...);
-        } else {
-            return std::make_unique<Simulator>(std::move(qc),
-                                               approx,
-                                               seed,
-                                               std::forward<Args>(args)...);
         }
+        return std::make_unique<Simulator>(std::move(qc),
+                                           approx,
+                                           seed,
+                                           std::forward<Args>(args)...);
     }
 }
 
 template<class Simulator, typename... Args>
-std::unique_ptr<Simulator> create_simulator_without_seed(const py::object& circ, Args&&... args) {
-    return create_simulator<Simulator>(circ, 1., 1, "fidelity", -1, std::forward<Args>(args)...);
+std::unique_ptr<Simulator> createSimulatorWithoutSeed(const py::object& circ, Args&&... args) {
+    return createSimulator<Simulator>(circ, 1., 1, "fidelity", -1, std::forward<Args>(args)...);
 }
 
 void getNumpyMatrixRec(const qc::MatrixDD& e, const std::complex<dd::fp>& amp, std::size_t i, std::size_t j, std::size_t dim, std::complex<dd::fp>* mat) {
@@ -81,22 +80,26 @@ void getNumpyMatrixRec(const qc::MatrixDD& e, const std::complex<dd::fp>& amp, s
 
     // base case
     if (e.isTerminal()) {
-        mat[i * dim + j] = c;
+        mat[i * dim + j] = c; // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
         return;
     }
 
-    std::size_t x = i | (1 << e.p->v);
-    std::size_t y = j | (1 << e.p->v);
+    const std::size_t x = i | (1 << e.p->v);
+    const std::size_t y = j | (1 << e.p->v);
 
     // recursive case
-    if (!e.p->e[0].w.approximatelyZero())
+    if (!e.p->e[0].w.approximatelyZero()) {
         getNumpyMatrixRec(e.p->e[0], c, i, j, dim, mat);
-    if (!e.p->e[1].w.approximatelyZero())
+    }
+    if (!e.p->e[1].w.approximatelyZero()) {
         getNumpyMatrixRec(e.p->e[1], c, i, y, dim, mat);
-    if (!e.p->e[2].w.approximatelyZero())
+    }
+    if (!e.p->e[2].w.approximatelyZero()) {
         getNumpyMatrixRec(e.p->e[2], c, x, j, dim, mat);
-    if (!e.p->e[3].w.approximatelyZero())
+    }
+    if (!e.p->e[3].w.approximatelyZero()) {
         getNumpyMatrixRec(e.p->e[3], c, x, y, dim, mat);
+    }
 }
 
 template<class Config = dd::DDPackageConfig>
@@ -110,7 +113,7 @@ void getNumpyMatrix(UnitarySimulator<Config>& sim, py::array_t<std::complex<dd::
         throw std::runtime_error("Provided matrix is not a square matrix.");
     }
 
-    std::size_t dim = 1 << (e.p->v + 1);
+    const std::size_t dim = 1 << (e.p->v + 1);
     if (static_cast<std::size_t>(rows) != dim) {
         throw std::runtime_error("Provided matrix does not have the right size.");
     }
@@ -118,16 +121,16 @@ void getNumpyMatrix(UnitarySimulator<Config>& sim, py::array_t<std::complex<dd::
     getNumpyMatrixRec(e, std::complex<dd::fp>{1.0, 0.0}, 0, 0, dim, dataPtr);
 }
 
-void dump_tensor_network(const py::object& circ, const std::string& filename) {
-    py::object QuantumCircuit       = py::module::import("qiskit").attr("QuantumCircuit");
-    py::object pyQasmQobjExperiment = py::module::import("qiskit.qobj").attr("QasmQobjExperiment");
+void dumpTensorNetwork(const py::object& circ, const std::string& filename) {
+    const py::object quantumCircuit       = py::module::import("qiskit").attr("QuantumCircuit");
+    const py::object pyQasmQobjExperiment = py::module::import("qiskit.qobj").attr("QasmQobjExperiment");
 
     std::unique_ptr<qc::QuantumComputation> qc = std::make_unique<qc::QuantumComputation>();
 
     if (py::isinstance<py::str>(circ)) {
         auto&& file1 = circ.cast<std::string>();
         qc->import(file1);
-    } else if (py::isinstance(circ, QuantumCircuit)) {
+    } else if (py::isinstance(circ, quantumCircuit)) {
         qc::qiskit::QuantumCircuit::import(*qc, circ);
     } else if (py::isinstance(circ, pyQasmQobjExperiment)) {
         qc::qiskit::QasmQobjExperiment::import(*qc, circ);
@@ -143,7 +146,7 @@ PYBIND11_MODULE(pyddsim, m) {
 
     // Circuit Simulator
     py::class_<CircuitSimulator<>>(m, "CircuitSimulator")
-            .def(py::init<>(&create_simulator<CircuitSimulator<>>),
+            .def(py::init<>(&createSimulator<CircuitSimulator<>>),
                  "circ"_a,
                  "approximation_step_fidelity"_a = 1.,
                  "approximation_steps"_a         = 1,
@@ -162,7 +165,7 @@ PYBIND11_MODULE(pyddsim, m) {
             .export_values();
 
     py::class_<HybridSchrodingerFeynmanSimulator<>>(m, "HybridCircuitSimulator")
-            .def(py::init<>(&create_simulator<HybridSchrodingerFeynmanSimulator<>, HybridSchrodingerFeynmanSimulator<>::Mode&, const std::size_t&>),
+            .def(py::init<>(&createSimulator<HybridSchrodingerFeynmanSimulator<>, HybridSchrodingerFeynmanSimulator<>::Mode&, const std::size_t&>),
                  "circ"_a,
                  "approximation_step_fidelity"_a = 1.,
                  "approximation_steps"_a         = 1,
@@ -205,9 +208,9 @@ PYBIND11_MODULE(pyddsim, m) {
             .def("__repr__", &PathSimulator<>::Configuration::toString);
 
     py::class_<PathSimulator<>>(m, "PathCircuitSimulator")
-            .def(py::init<>(&create_simulator_without_seed<PathSimulator<>, PathSimulator<>::Configuration&>),
+            .def(py::init<>(&createSimulatorWithoutSeed<PathSimulator<>, PathSimulator<>::Configuration&>),
                  "circ"_a, "config"_a = PathSimulator<>::Configuration())
-            .def(py::init<>(&create_simulator_without_seed<PathSimulator<>, PathSimulator<>::Configuration::Mode&, const std::size_t&, const std::size_t&, const std::list<std::size_t>&, const std::size_t&>),
+            .def(py::init<>(&createSimulatorWithoutSeed<PathSimulator<>, PathSimulator<>::Configuration::Mode&, const std::size_t&, const std::size_t&, const std::list<std::size_t>&, const std::size_t&>),
                  "circ"_a, "mode"_a = PathSimulator<>::Configuration::Mode::Sequential, "bracket_size"_a = 2, "starting_point"_a = 0, "gate_cost"_a = std::list<std::size_t>{}, "seed"_a = 0)
             .def("set_simulation_path", py::overload_cast<const PathSimulator<>::SimulationPath::Components&, bool>(&PathSimulator<>::setSimulationPath))
             .def("get_number_of_qubits", &PathSimulator<>::getNumberOfQubits)
@@ -223,7 +226,7 @@ PYBIND11_MODULE(pyddsim, m) {
             .export_values();
 
     py::class_<UnitarySimulator<>>(m, "UnitarySimulator")
-            .def(py::init<>(&create_simulator<UnitarySimulator<>, UnitarySimulator<>::Mode&>),
+            .def(py::init<>(&createSimulator<UnitarySimulator<>, UnitarySimulator<>::Mode&>),
                  "circ"_a,
                  "approximation_step_fidelity"_a = 1.,
                  "approximation_steps"_a         = 1,
@@ -241,7 +244,7 @@ PYBIND11_MODULE(pyddsim, m) {
     // Miscellaneous functions
     m.def("get_matrix", &getNumpyMatrix<>, "sim"_a, "mat"_a);
 
-    m.def("dump_tensor_network", &dump_tensor_network, "dump a tensor network representation of the given circuit",
+    m.def("dumpTensorNetwork", &dumpTensorNetwork, "dump a tensor network representation of the given circuit",
           "circ"_a, "filename"_a);
 
 #define STRINGIFY(x) #x
