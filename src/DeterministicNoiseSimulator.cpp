0c4edc193c1b1a4e4009c53e8decb83448ff6c2f
Stefan Hillmich <stefan.hillmich@scch.at>
2023-07-21 12:55:13 +0200

    Silence false-positives

--- src/DeterministicNoiseSimulator.cpp
+++ src/DeterministicNoiseSimulator.cpp
@@ -54,12 +54,12 @@ std::map<std::string, std::size_t> DeterministicNoiseSimulator<Config>::sampleFr
     for (const auto& [state, prob]: resultProbabilityMap) {
         weights.emplace_back(prob);
     }
-    std::discrete_distribution<std::size_t> d(weights.begin(), weights.end());
+    std::discrete_distribution<std::size_t> d(weights.begin(), weights.end()); // NOLINT(misc-const-correctness) false-positive
 
     //Sample n shots elements from the prob distribution
     std::map<std::size_t, std::size_t> results;
     for (size_t n = 0; n < shots; ++n) {
-        ++results[d(Simulator<Config>::mt)];
+        ++results[d(Simulator<Config>::mt)]; // NOLINT(misc-const-correctness) false-positive
     }
 
     // Create the final map containing the measurement results and the corresponding shots
