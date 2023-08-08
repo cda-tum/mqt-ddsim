31207153a7241af6818cd3ae8e43db56c620a0e9
burgholzer <burgholzer@me.com>
2023-08-08 16:07:17 +0200

    ♻️ significantly simplify Provider

--- mqt/ddsim/provider.py
+++ mqt/ddsim/provider.py
@@ -11,37 +11,22 @@ from mqt.ddsim.unitarysimulator import UnitarySimulatorBackend
 
 
 class DDSIMProvider(ProviderV1):
-    _BACKENDS = None
-
-    def __init__(self):
-        if DDSIMProvider._BACKENDS is None:
-            DDSIMProvider._BACKENDS = [
-                ("qasm_simulator", QasmSimulatorBackend, None, None),
-                ("statevector_simulator", StatevectorSimulatorBackend, None, None),
-                ("hybrid_qasm_simulator", HybridQasmSimulatorBackend, None, None),
-                ("hybrid_statevector_simulator", HybridStatevectorSimulatorBackend, None, None),
-                ("path_sim_qasm_simulator", PathQasmSimulatorBackend, None, None),
-                ("path_sim_statevector_simulator", PathStatevectorSimulatorBackend, None, None),
-                ("unitary_simulator", UnitarySimulatorBackend, None, None),
-            ]
+    _BACKENDS = (
+        ("qasm_simulator", QasmSimulatorBackend),
+        ("statevector_simulator", StatevectorSimulatorBackend),
+        ("hybrid_qasm_simulator", HybridQasmSimulatorBackend),
+        ("hybrid_statevector_simulator", HybridStatevectorSimulatorBackend),
+        ("path_sim_qasm_simulator", PathQasmSimulatorBackend),
+        ("path_sim_statevector_simulator", PathStatevectorSimulatorBackend),
+        ("unitary_simulator", UnitarySimulatorBackend),
+    )
 
     def get_backend(self, name=None, **kwargs):
         return super().get_backend(name=name, **kwargs)
 
     def backends(self, name=None, filters=None, **kwargs):
-        # pylint: disable=arguments-differ
-        # Instantiate a new backend instance so if config options
-        # are set they will only last as long as that backend object exists
         backends = []
-        for backend_name, backend_cls, method, device in self._BACKENDS:
-            opts = {"provider": self}
-            if method is not None:
-                opts["method"] = method
-            if device is not None:
-                opts["device"] = device
+        for backend_name, backend_cls in self._BACKENDS:
             if name is None or backend_name == name:
-                backends.append(backend_cls(**opts))
+                backends.append(backend_cls())
         return filter_backends(backends, filters=filters, **kwargs)
-
-    def __str__(self):
-        return "DDSIMProvider"
