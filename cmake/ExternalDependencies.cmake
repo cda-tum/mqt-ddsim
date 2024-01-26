857001e8cf5b92db6939ee1091b71785d18b3899
burgholzer <burgholzer@me.com>
2024-01-26 21:24:09 +0100

    ➕ explicitly add googletest dependency
    Signed-off-by: burgholzer <burgholzer@me.com>

---
+++ cmake/ExternalDependencies.cmake
@@ -0,0 +1,30 @@
+# Declare all external dependencies and make sure that they are available.
+
+include(FetchContent)
+set(FETCH_PACKAGES "")
+
+if(BUILD_MQT_DDSIM_TESTS)
+  set(gtest_force_shared_crt
+      ON
+      CACHE BOOL "" FORCE)
+  set(GTEST_VERSION
+      1.14.0
+      CACHE STRING "Google Test version")
+  set(GTEST_URL
+      https://github.com/google/googletest/archive/refs/tags/v${GTEST_VERSION}.tar.gz
+  )
+  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
+    FetchContent_Declare(googletest URL ${GTEST_URL} FIND_PACKAGE_ARGS
+                                        ${GTEST_VERSION} NAMES GTest)
+    list(APPEND FETCH_PACKAGES googletest)
+  else()
+    find_package(googletest ${GTEST_VERSION} QUIET NAMES GTest)
+    if(NOT googletest_FOUND)
+      FetchContent_Declare(googletest URL ${GTEST_URL})
+      list(APPEND FETCH_PACKAGES googletest)
+    endif()
+  endif()
+endif()
+
+# Make all declared dependencies available.
+FetchContent_MakeAvailable(${FETCH_PACKAGES})
