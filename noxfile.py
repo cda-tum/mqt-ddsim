60fe831af2bed95a5c3ef760528477e48978e1d9
burgholzer <burgholzer@me.com>
2024-10-05 10:56:31 +0200

    ⬆️ update Python dependency lower bounds
    Signed-off-by: burgholzer <burgholzer@me.com>

--- noxfile.py
+++ noxfile.py
@@ -27,9 +27,8 @@ PYTHON_ALL_VERSIONS = ["3.9", "3.10", "3.11", "3.12"]
 # not available via wheels on PyPI (i.e., only as source distributions).
 BUILD_REQUIREMENTS = [
     "scikit-build-core>=0.10.1",
-    "setuptools_scm>=7",
+    "setuptools_scm>=8.1",
     "pybind11>=2.13.5",
-    "wheel>=0.40",  # transitive dependency of pytest on Windows
 ]
 
 if os.environ.get("CI", None):
