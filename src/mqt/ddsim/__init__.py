acbb34a62d7e122ba8ce0443e6e301ae55c62e71
burgholzer <burgholzer@me.com>
2025-03-04 15:41:43 +0100

    🏁 patch dll directory on Windows
    Signed-off-by: burgholzer <burgholzer@me.com>

--- src/mqt/ddsim/__init__.py
+++ src/mqt/ddsim/__init__.py
@@ -2,6 +2,24 @@
 
 from __future__ import annotations
 
+import sys
+
+# under Windows, make sure to add the appropriate DLL directory to the PATH
+if sys.platform == "win32":
+
+    def _dll_patch() -> None:
+        """Add the DLL directory to the PATH."""
+        import os
+        import sysconfig
+        from pathlib import Path
+
+        site_packages = Path(sysconfig.get_paths()["purelib"])
+        bin_dir = site_packages / "mqt" / "core" / "bin"
+        os.add_dll_directory(str(bin_dir))
+
+    _dll_patch()
+    del _dll_patch
+
 from ._version import version as __version__
 from .provider import DDSIMProvider
 from .pyddsim import (
